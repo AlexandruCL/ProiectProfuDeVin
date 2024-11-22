28f5f22901d570814f9fea4e09734e116d7332ad
patric1304 <patric.3pop@gmail.com>
2024-11-22 11:38:21 +0200

    Update views.py

--- my_project/my_app/views.py
+++ my_project/my_app/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Wines, Cart, CartItem
 from django.contrib import messages
 
+
 @login_required
 def add_to_cart(request, wine_id):
     wine = get_object_or_404(Wines, ID=wine_id)
