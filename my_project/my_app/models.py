578e2b4bf4ddaf1a580b8412e0fb4be49ea7d96c
Alexandru CL <laur.cristea@student.upt.ro>
2024-11-22 12:17:20 +0200

    asdadsa

--- my_project/my_app/models.py
+++ my_project/my_app/models.py
@@ -4,16 +4,17 @@ from django.contrib.auth.models import User
 
 class Wines(models.Model):
     name = models.CharField(max_length=100)
-    year = models.IntegerField()
+    type = models.CharField(max_length=100)
+    year = models.IntegerField(null=True)
     grapes = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
-    region = models.CharField(max_length=100)
+    region = models.CharField(max_length=100, null=True)
     description = models.TextField()
     quantity = models.IntegerField(null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, null=True)
     ID = models.AutoField(primary_key=True, editable=False)
     def __str__(self):
-        return f'{self.name} | {self.year} | {self.grapes} | {self.country} | {self.region} | {self.description} | {self.quantity} | {self.price} | {self.ID}'
+        return f'{self.name} | {self.year} | {self.grapes} | {self.country} | {self.region} | {self.description} | {self.price} | {self.quantity} | {self.ID}'
 
 class Cart(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)  # Enforce unique constraint
--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_database_name',
+        'USER': 'your_database_user',
+        'PASSWORD': 'your_database_password',
+        'HOST': 'your_database_host',  # e.g., 'your-database-host.amazonaws.com'
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
 
